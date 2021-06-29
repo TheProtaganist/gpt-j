075a9ea2039f08ef2234d36060a94241454c2a16
TheProtaganist <45369668+TheProtaganist@users.noreply.github.com>
2021-06-29 08:10:47 -0400

    Update gptj.py

--- gptj.py
+++ gptj.py
@@ -8,8 +8,8 @@ class GPTJ:
 
     def generate(self, context, token_max_length, temperature, top_p):
         assert isinstance(token_max_length, int), "Max token most be integer value"
-        assert isinstance(temperature, float), "Max token most be float value"
-        assert isinstance(top_p, float), "Max token most be float value"
+        assert isinstance(temperature, float), "temperature most be float value"
+        assert isinstance(top_p, float), "top_p most be float value"
         payload = {"context": str(context), "token_max_length": token_max_length, "temperature": temperature,"top_p": top_p}
         URL = requests.post("http://34.90.255.118:5000/generate", params=payload)
         text = URL.json()
