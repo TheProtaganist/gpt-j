9866e49d32d700f906361b0750fa49f23e8df6dd
TheProtaganist <45369668+TheProtaganist@users.noreply.github.com>
2021-07-08 16:34:15 -0400

    Update gptj.py

--- gptj.py
+++ gptj.py
@@ -1,16 +1,11 @@
 import requests
 
 
-class GPTJ:
-
-    def __init__(self):
-        pass
-
-    def generate(self, context, token_max_length, temperature, top_p):
-        assert isinstance(token_max_length, int), "Max token most be integer value"
-        assert isinstance(temperature, float), "temperature most be float value"
-        assert isinstance(top_p, float), "top_p most be float value"
-        payload = {"context": str(context), "token_max_length": token_max_length, "temperature": temperature,"top_p": top_p}
-        URL = requests.post("http://api.vicgalle.net:5000/generate", params=payload)
-        text = URL.json()
-        return str(text["text"])
+def generate(context, token_max_length, temperature, top_p):
+    assert isinstance(token_max_length, int), "Max token most be integer value"
+    assert isinstance(temperature, float), "temperature most be float value"
+    assert isinstance(top_p, float), "top_p most be float value"
+    payload = {"context": str(context), "token_max_length": token_max_length, "temperature": temperature, "top_p": top_p}
+    URL = requests.post("http://api.vicgalle.net:5000/generate", params=payload)
+    text = URL.json()
+    return str(text["text"])
