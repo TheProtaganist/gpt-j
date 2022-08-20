ff385d00c423c35cdeec2ad151555c4f0e87bcf6
TheProtagonist <45369668+TheProtaganist@users.noreply.github.com>
2022-08-20 15:20:58 -0400

    Update basic_usage_template.py

--- basic_usage_template.py
+++ basic_usage_template.py
@@ -15,9 +15,17 @@ temperature = 0.6
 # top probability is an alternative way to control the randomness of the model
 top_probability = 1.0
 
+# top_k is the number of top responses to return
+# This is the number of responses the model will return
+top_k = 1
+
+# Rep is the number of times the model will repeat itself
+# This is the number of times the model will repeat itself when generating a response
+rep = 1.0
+
 # Here you set query equal to the desired values
 # Note values higher that 512 tend to take more time to generate
-res = simple_completion(prompt, length=Max, temp=temperature, top=top_probability, top_k=k)
+res = simple_completion(prompt, length=Max, temp=temperature, top=top_probability, top_k=k, rep=rep)
 
 # Finally we print the result
 print(res)
